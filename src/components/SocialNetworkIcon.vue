27ed3cfb6e62e1fef5590ea321b31bfd1033f811
Marcelo Cueto <cueto@live.cl>
2021-10-02 15:56:56 -0300

    Create SocialnetworkIcon component #1

---
+++ src/components/SocialNetworkIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <a class="px-4 mx-0 my-2 col-6 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0" :href=linkTo>
+        <font-awesome-icon :icon="[iconType, icon]" size="lg" fixed-width />
+        {{ label }}
+    </a>
+</template>
+
+
+<script>
+export default {
+    name: 'SocialNetworkIcon',
+    props: [
+        'iconType',
+        'icon',
+        'linkTo',
+        'label',
+    ]
+}
+</script>
+
+<style lang="scss">
+</style>
