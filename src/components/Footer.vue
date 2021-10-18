cfb3ec7bce25a7bc18d32c8c069dc90b57963578
Marcelo Cueto <cueto@live.cl>
2021-10-17 23:15:09 -0300

    Add padding to footer component #1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="my-2">
+  <footer class="my-2 p-2">
     <a href="https://github.com/agapanto/under-construction-gridsome-starter" class="text-decoration-none">under-construction-gridsome-starter</a> developed by<br>
     <a href="https://agapanto.cl" class="text-decoration-none">agapanto comunicación digital</a>
   </footer>
