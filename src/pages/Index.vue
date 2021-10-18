0ef8d5539eb059f01cd7a94d6d8c5c266a63b75c
Marcelo Cueto <cueto@live.cl>
2021-10-17 22:45:24 -0300

    Update index page classes for bootstrap 5 ones #1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,13 +2,13 @@
   <Layout>
     <div class="container">
 
-      <div class="row py-4 mb-4">
+      <div class="row py-4 mt-4">
         <div class="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
           <g-image class="img-fluid" src="~/assets/images/software-development.png"/>
         </div>
       </div>
 
-      <div class="row">
+      <div class="row pt-4">
         <div class="col-12 hello">
           <h1>
             This website is under construction
@@ -21,7 +21,7 @@
       </div>
 
       <div class="row">
-        <div class="col-12 offset-0 col-md-8 offset-md-2 col-lg-10 offset-lg-1 social-icons text-left my-4">
+        <div class="col-12 offset-0 col-md-8 offset-md-2 col-lg-10 offset-lg-1 social-icons text-start my-4">
           <SocialNetworks />
         </div>
       </div>
