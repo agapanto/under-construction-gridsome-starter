46c2eb60ecf1324b554ebf08e2f8ed94faf65738
Marcelo Cueto <cueto@live.cl>
2021-10-17 23:12:36 -0300

    Update Header component #1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,41 @@
 <template>
-  <header class="header">
-    <b-navbar class="p-0" toggleable="md" type="light">
-        <b-navbar-brand>
-        <g-link class="logo-home logo-desktop" to="/">{{ $static.metadata.siteName }}</g-link>
-        <g-link class="logo-home logo–mobile" to="/"><img class="logo-image" alt="logo" src="../assets/images/logo.png"></g-link>
-      </b-navbar-brand>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav class="ml-auto">
-          <g-link class="nav--link" to="/about">About</g-link>
-          <g-link class="nav--link" to="/blog">Blog</g-link>
-          <g-link class="nav--link" to="/contact">Contact</g-link>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </header>
+  <div>
+    <nav class="py-2 bg-agapanto-purple bg-shadowed">
+      <div class="container d-flex flex-wrap">
+        <ul class="nav me-auto">
+          <li class="nav-item">
+            <g-link to="/" class="nav-link link-dark px-2 me-4">Home</g-link>
+          </li>
+          <li class="nav-item">
+            <g-link to="/about" class="nav-link link-dark px-2 mx-4">About us</g-link>
+          </li>
+          <li class="nav-item">
+            <g-link to="/contact-us" class="nav-link link-dark px-2 mx-4">Contact us</g-link>
+          </li>
+        </ul>
+        <ul class="nav">
+          <li class="nav-item btn-group">
+            <a href="#" class="nav-link px-2 btn btn-sm btn-purple rounded-3 text-decoration-none">
+              <font-awesome-icon :icon="['fas', 'user-circle']" size="lg" fixed-width /> My Account
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- header class="py-3 mb-4 bg-light border-bottom">
+      <div class="container d-flex flex-wrap justify-content-center">
+        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+          <span class="fs-4">Double header</span>
+        </a>
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+      </div>
+    </header-->
+
+  </div>
 </template>
 
 <static-query>
@@ -27,57 +48,31 @@ query {
 
 <style lang="scss">
 nav {
-  width: 100%;
-  padding-top: 1rem;
+  &.bg-shadowed {
+    box-shadow: inset 0 -0.44rem 0.8rem darken(#6B3973, 20);
+    // border-top: 2px solid darken(#6B3973, 20);
+    border-bottom: 2px solid darken(#6B3973, 20);
+  }
 }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
+.nav-link.active--exact {
+  color: white !important;
+  text-shadow: 0 0 1.8rem lighten(#6B3973, 40);
 }
 
-.logo-home {
-  font-size: 24px;
-  max-width: 75%;
-  font-weight: bold;
+.bg-agapanto-purple {
+  background-color: darken(#6B3973, 12);
+}
+
+.btn-purple {
+  text-shadow: none;
+  color: #fff !important;
+  background-color: lighten(#6B3973, 0) !important;
+  border: 1px solid darken(#6B3973, 12) !important;
+  box-shadow: 0 0 0.2rem darken(#6B3973, 12) !important;
+
   &:hover {
-    text-decoration: none;
+    background-color: lighten(#6B3973, 6) !important;
   }
-}
-
-.logo-desktop {
-  @media(max-width: 419px) {
-    display: none;
-  }
-  @media (min-width: 420px) {
-    display: default;
-  }
-}
-
-.logo–mobile {
-  @media(max-width: 419px) {
-    display: default;
-  }
-  @media (min-width: 420px) {
-    display: none;
-  }
-}
-
-.logo-image {
-  width: 40px;
-}
-
-.nav--link {
-  margin-left: 20px;
-  @media (max-width: 419px) {
-    margin: 0;
-    padding: 15px 10px 10px 0;
-  }
-}
-
-.active--exact {
-  font-weight: bold;
 }
 </style>
