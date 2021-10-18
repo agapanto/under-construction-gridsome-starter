2568c3a88fc196c11b01a6041dbd0d5fdbeb1821
Marcelo Cueto <cueto@live.cl>
2021-10-17 23:14:09 -0300

    Remove text decoration on SocialNetworkIcon component  #1

--- src/components/SocialNetworkIcon.vue
+++ src/components/SocialNetworkIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="px-4 mx-0 my-2 col-6 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0" :href=linkTo>
+    <a class="px-4 mx-0 my-2 col-6 col-lg-4 offset-lg-0 col-xl-3 offset-xl-0 text-decoration-none" :href=linkTo>
         <font-awesome-icon :icon="[iconType, icon]" size="lg" fixed-width />
         {{ label }}
     </a>
